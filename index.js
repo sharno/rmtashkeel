caf64cab642c42e57bd07d58e0342d14f0100185
sharno <sharnoby3@gmail.com>
2024-01-31 22:10:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// @ts-check
+
+function rmTashkeel() {
+  let text = document.getElementById("input")?.value;
+  text = text.replace(/([^\u0621-\u064A\u0660-\u0669a-zA-Z 0-9])/g, "");
+  const output = document.getElementById("output");
+  output.value = text;
+}
